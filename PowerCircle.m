90b9cf8499da00c685964f624997c910ae85343c
TroelsKT <tomba666@hotmail.com>
2018-04-13 13:31:12 +0200

    Updated Matlab Scripts

--- PowerCircle.m
+++ PowerCircle.m
@@ -1,5 +1,9 @@
 clc; clear; close all;
 
+%-94dBm is antenna receiver sensitivity
+%-84dBm is chosen to give room for unexpected noise sources!
+acc_dBm = -84;            %Accepted received power
+
 R = 1;                    %Measurement Shunt-Resistance
 
 I_tx_max = 1*10.^(-3);    %Transmit current
@@ -27,8 +31,8 @@ d0     = 0.01167;    %(meter) estimation of the Far Field distance for the Telos
 % Prcvd_min = (Ptx_Antenna_min*Gt*Gr*lambda^(2))/(area*d0^(2)*L); %Recieved signal power at -24dBm Ptx
 
 N = 200; %meter from the base station "half of a racetrack width"
-x = linspace(-N,N,N+1);  
-y = linspace(-N,N,N+1);  
+x = linspace(-N,N,N*2+1);  
+y = linspace(-N,N,N*2+1);  
 [X,Y] = meshgrid(x,y);
 %% 0dBm = max power, -24dBm = min power
 %  AIR = 2
@@ -36,9 +40,10 @@ gammaAIR = 2;
 % Office = 5.5 (2% of the distance) for every 5m one wall of thickness 30cm exist! 
 gammaOffice = (2*94+5.5*6)/100; %2.21
 
+
 %BaseStation
-d = sqrt(X.^(2)+Y.^(2)); %euclidean distance
-d(N/2+1,N/2+1) = 1;      %to avoid inf number in the center!
+d = sqrt(X.^(2)+Y.^(2));    %euclidean distance
+d((N*2)/2+1,(N*2)/2+1) = 1; %to avoid inf number in the center!
 
 PAir_W     = Ptx_max.*((d0./d).^gammaAIR); %Watt
 PAir       = 10*log10(PAir_W/0.001);       %dBm
@@ -46,77 +51,83 @@ PAir_W_min = Ptx_min.*((d0./d).^gammaAIR); %Watt
 PAir_min   = 10*log10(PAir_W_min/0.001);   %dBm
 
 POffice_W     = Ptx_max.*((d0./d).^gammaOffice); %Watt
-POffice       = 10*log10(PAir_W/0.001);          %dBm
+POffice       = 10*log10(POffice_W/0.001);       %dBm
 POffice_W_min = Ptx_min.*((d0./d).^gammaOffice); %Watt
-POffice_min   = 10*log10(PAir_W_min/0.001);      %dBm
+POffice_min   = 10*log10(POffice_W_min/0.001);   %dBm
 
-%NorthSation
-dNorth = sqrt((X-50).^(2)+Y.^(2));       %euclidean distance
-dNorth(N/2+1,N/2+26) = 1;                %to avoid inf number in the center!
-dNorthmin = sqrt((X-4).^(2)+Y.^(2));     %euclidean distance
-dNorthmin(N/2+1,N/2+3) = 1;              %to avoid inf number in the center!
-dNorthOffice = sqrt((X-24).^(2)+Y.^(2)); %euclidean distance
-dNorthOffice(N/2+1,N/2+13) = 1;          %to avoid inf number in the center!
+%NorthStation
+%NorthStation placed half a track height away from basestation
+dNorth = sqrt((X-40).^(2)+Y.^(2));      %euclidean distance     
+dNorth((N*2)/2+1,(N*2)/2+41) = 1;       %to avoid inf number in the center!       
+dNorthOffice = sqrt((X-20).^(2)+Y.^(2)); 
+dNorthOffice((N*2)/2+1,(N*2)/2+21) = 1; 
+dNorthmin = sqrt((X-5).^(2)+Y.^(2));     
+dNorthmin((N*2)/2+1,(N*2)/2+6) = 1;              
+dNorthOfficemin = sqrt((X-3).^(2)+Y.^(2));
+dNorthOfficemin((N*2)/2+1,(N*2)/2+4) = 1;
 
-PAirNorth       = 10*log10((Ptx_max.*((d0./dNorth).^gammaAIR)./0.001));          %dBm
-PAirNorth_W     = Ptx_max.*((d0./dNorth).^gammaAIR);                           %Watt
-PAirNorth_W_min = Ptx_min.*((d0./dNorthmin).^gammaAIR);                        %Watt
-POfficeNorth_W  = Ptx_max.*((d0./dNorthOffice).^gammaOffice);                  %Watt
-POfficeNorth_W_min  = Ptx_min.*((d0./dNorthOffice).^gammaOffice);                  %Watt
+PAirNorth       = 10*log10((Ptx_max.*((d0./dNorth).^gammaAIR)./0.001)); %dBm
+PAirNorth_W     = Ptx_max.*((d0./dNorth).^gammaAIR);                    %Watt
+PAirNorth_W_min = Ptx_min.*((d0./dNorthmin).^gammaAIR);                 %Watt
+POfficeNorth_W  = Ptx_max.*((d0./dNorthOffice).^gammaOffice);           %Watt
+POfficeNorth_W_min  = Ptx_min.*((d0./dNorthOfficemin).^gammaOffice);    %Watt
 
 %SouthStation
-dSouth = sqrt((X+50).^(2)+Y.^(2));       %euclidean distance
-dSouth(N/2+1,N/2-24) = 1;                %to avoid inf number in the center!
-dSouthmin = sqrt((X+4).^(2)+Y.^(2));     %euclidean distance
-dSouthmin(N/2+1,N/2-1) = 1;              %to avoid inf number in the center!
-dSouthOffice = sqrt((X+24).^(2)+Y.^(2)); %euclidean distance
-dSouthOffice(N/2+1,N/2-11) = 1;          %to avoid inf number in the center!
+%SouthStation placed half a track height away from basestation
+dSouth = sqrt((X+40).^(2)+Y.^(2));        %euclidean distance
+dSouth((N*2)/2+1,(N*2)/2-39) = 1;         %to avoid inf number in the center!
+dSouthOffice = sqrt((X+20).^(2)+Y.^(2)); 
+dSouthOffice((N*2)/2+1,(N*2)/2-19) = 1;  
+dSouthmin = sqrt((X+5).^(2)+Y.^(2));     
+dSouthmin((N*2)/2+1,(N*2)/2-4) = 1;      
+dSouthminOffice = sqrt((X+3).^(2)+Y.^(2));
+dSouthminOffice((N*2)/2+1,(N*2)/2-2) = 1;
 
-PAirSouth          = 10*log10((Ptx_max.*((d0./dSouth).^gammaAIR)./0.001));          %dBm
-PAirSouth_W        = Ptx_max.*((d0./dSouth).^gammaAIR);                           %Watt
-PAirSouth_W_min    = Ptx_min.*((d0./dSouthmin).^gammaAIR);                        %Watt
-POfficeSouth_W     = Ptx_max.*((d0./dSouthOffice).^gammaOffice);                  %Watt
-POfficeSouth_W_min = Ptx_min.*((d0./dSouthOffice).^gammaOffice);                  %Watt
-
-%Combination of stations
-PAIRCombined     = 10*log10((PAir_W + PAirNorth_W + PAirSouth_W)./0.001);             %dBm
-PAIRCombined_min = 10*log10((PAir_W_min + PAirNorth_W_min + PAirSouth_W_min)./0.001); %dBm
-POfficeCombined  = 10*log10((POffice_W + POfficeNorth_W + POfficeSouth_W)./0.001);    %dBm
-POfficeCombined_min  = 10*log10((POffice_W_min + POfficeNorth_W_min + POfficeSouth_W_min)./0.001);    %dBm
+PAirSouth          = 10*log10((Ptx_max.*((d0./dSouth).^gammaAIR)./0.001)); %dBm
+PAirSouth_W        = Ptx_max.*((d0./dSouth).^gammaAIR);                    %Watt
+PAirSouth_W_min    = Ptx_min.*((d0./dSouthmin).^gammaAIR);                 %Watt
+POfficeSouth_W     = Ptx_max.*((d0./dSouthOffice).^gammaOffice);           %Watt
+POfficeSouth_W_min = Ptx_min.*((d0./dSouthminOffice).^gammaOffice);        %Watt
+ 
+%Combination of stations in dBm
+PAIRCombined         = 10*log10((PAir_W + PAirNorth_W + PAirSouth_W)./0.001);                          
+PAIRCombined_min     = 10*log10((PAir_W_min + PAirNorth_W_min + PAirSouth_W_min)./0.001);              
+POfficeCombined      = 10*log10((POffice_W + POfficeNorth_W + POfficeSouth_W)./0.001);                 
+POfficeCombined_min  = 10*log10((POffice_W_min + POfficeNorth_W_min + POfficeSouth_W_min)./0.001); 
 
 %% Plotting Signal Range in AIR for 0dBm
 figure(1)
 hold on
 pcolor(x,y,PAir)
-title('0dBm transmit power decay in AIR. BaseStation')
-xlabel('-200m < BaseStation < 200m') % x-axis label
-ylabel('-200m < BaseStation < 200m') % y-axis label
+title({'LOGPATH RECEIVED SIGNAL PLOT';'BASE-Station, AIR, Trx = 0dBm'})
+xlabel('-200m < BaseStation < 200m')
+ylabel('-200m < BaseStation < 200m')
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]); 
 colormap([0 0 0; jet]);
 colorbar;
 
 %Ellipse
 a=198; % horizontal radius
-b=40; % vertical radius
-x0=0; % x0,y0 ellipse centre coordinates
+b=40;  % vertical radius
+x0=0;  % x0,y0 ellipse centre coordinates
 y0=0;
 t=-pi:0.01:pi;
 x_ellipse=x0+a*cos(t);
 y_ellipse=y0+b*sin(t);
 plot(x_ellipse,y_ellipse,'r')
-legend('log10-Distance path model', 'RaceTrack');
+legend('log10 distance path model', 'RaceTrack, "H=80m, L=400m"');
 hold off
 
 figure(2)
 subplot(3,1,1)
 hold on
 pcolor(x,y,PAirSouth)
-title('0dBm transmit power decay in AIR. SouthStation')
+title({'LOGPATH RECEIVED SIGNAL PLOT';'SOUTH-Station, AIR, Trx = 0dBm'})
 xlabel('-200m < BaseStation < 200m') % x-axis label
 ylabel('-200m < BaseStation < 200m') % y-axis label
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 plot(x_ellipse,y_ellipse,'r')
 hold off
@@ -124,11 +135,11 @@ hold off
 subplot(3,1,2)
 hold on
 pcolor(x,y,PAir)
-title('0dBm transmit power decay in AIR. BaseStation')
-xlabel('-200m < BaseStation < 200m') % x-axis label
-ylabel('-200m < BaseStation < 200m') % y-axis label
+title({'LOGPATH RECEIVED SIGNAL PLOT';'BASE-Station, AIR, Trx = 0dBm'})
+xlabel('-200m < BaseStation < 200m')
+ylabel('-200m < BaseStation < 200m')
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 plot(x_ellipse,y_ellipse,'r')
 hold off
@@ -136,11 +147,11 @@ hold off
 subplot(3,1,3)
 hold on
 pcolor(x,y,PAirNorth)
-title('0dBm transmit power decay in AIR. NorthStation')
-xlabel('-200m < BaseStation < 200m') % x-axis label
-ylabel('-200m < BaseStation < 200m') % y-axis label
+title({'LOGPATH RECEIVED SIGNAL PLOT';'NORTH-Station, AIR, Trx = 0dBm'})
+xlabel('-200m < BaseStation < 200m')
+ylabel('-200m < BaseStation < 200m')
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 plot(x_ellipse,y_ellipse,'r')
 hold off
@@ -148,91 +159,132 @@ hold off
 figure(3)
 hold on
 pcolor(x,y,PAIRCombined)
-title('0dBm transmit power decay in AIR. Combined Stations')
-xlabel('-200m < BaseStation < 200m') % x-axis label
-ylabel('-200m < BaseStation < 200m') % y-axis label
+title({'LOGPATH RECEIVED SIGNAL PLOT';'COMBINED-Stations, AIR, Trx = 0dBm'})
+xlabel('-200m < BaseStation < 200m')
+ylabel('-200m < BaseStation < 200m')
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 colorbar;
 plot(x_ellipse,y_ellipse,'r')
-legend('log10-Distance path model', 'RaceTrack');
+legend('log10 distance path model', 'RaceTrack, "H=80m, L=400m"');
 hold off
 
 
 %% Plotting Signal Range in AIR for -24dBm
+scale = 25;
 figure(4)
 hold on
-pcolor(x,y,PAir_min)
-title('-24dBm transmit power decay in AIR. BaseStation')
-xlabel('-15m < BaseStation < 15m') % x-axis label
-ylabel('-15m < BaseStation < 15m') % y-axis label
+pcolor(x(N+1-scale:N+1+scale),y(N+1-scale:N+1+scale),PAir_min(N+1-scale:N+1+scale,N+1-scale:N+1+scale))
+title({'LOGPATH RECEIVED SIGNAL PLOT';'BASE-Station, AIR, Trx = -24dBm'})
+xlabel('-25m < BaseStation < 25m')
+ylabel('-25m < BaseStation < 25m')
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 colorbar;
 
 %Ellipse
-a=10; % horizontal radius
-b=2; % vertical radius
+a=15; % horizontal radius
+b=3;  % vertical radius 
 x0=0; % x0,y0 ellipse centre coordinates
 y0=0;
 t=-pi:0.01:pi;
 x_ellipse=x0+a*cos(t);
 y_ellipse=y0+b*sin(t);
 plot(x_ellipse,y_ellipse,'r')
-legend('log10-Distance path model', 'RaceTrack');
+legend('log10 distance path model', 'RaceTrack, "H=6m, L=30m"');
 hold off
 
 figure(5)
 hold on
-pcolor(x,y,PAIRCombined_min)
-title('-24dBm transmit power decay in AIR. Combined Stations')
-xlabel('-15m < BaseStation < 15m') % x-axis label
-ylabel('-15m < BaseStation < 15m') % y-axis label
+pcolor(x(N+1-scale:N+1+scale),y(N+1-scale:N+1+scale),PAIRCombined_min(N+1-scale:N+1+scale,N+1-scale:N+1+scale))
+title({'LOGPATH RECEIVED SIGNAL PLOT';'COMBINED-Stations, AIR, Trx = -24dBm'})
+xlabel('-25m < BaseStation < 25m')
+ylabel('-25m < BaseStation < 25m')
 shading interp;
-set(gca, 'clim', [-94 0]);
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 colorbar;
 plot(x_ellipse,y_ellipse,'r')
-legend('log10-Distance path model', 'RaceTrack');
+legend('log10 distance path model', 'RaceTrack, "H=6m, L=30m"');
 hold off
 
 %% Plotting Signal Range in Shannon Engineering Building for 0dBm
 figure(6)
 hold on
 pcolor(x,y,POffice)
-title('0dBm transmit power decay in office. BaseStation')
-xlabel('-42m < BaseStation < 42m') % x-axis label
-ylabel('-42m < BaseStation < 42m') % y-axis label
+title({'LOGPATH RECEIVED SIGNAL PLOT';'BASE-Station, OFFICE, Trx = 0dBm'})
+xlabel('-100m < BaseStation < 100m')
+ylabel('-100m < BaseStation < 100m')
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 colorbar;
 
 %Ellipse
-a=60; % horizontal radius
-b=15; % vertical radius
+a =100; % horizontal radius
+b = 20; % vertical radius
+x0=  0; % x0,y0 ellipse centre coordinates
+y0=  0;
+t=-pi:0.01:pi;
+x_ellipse=x0+a*cos(t);
+y_ellipse=y0+b*sin(t);
+plot(x_ellipse,y_ellipse,'r')
+legend('log10 distance path model', 'RaceTrack, "H=40m, L=100m"');
+hold off
+
+figure(7)
+hold on
+pcolor(x,y,POfficeCombined)
+title({'LOGPATH RECEIVED SIGNAL PLOT';'COMBINED-Stations, OFFICE, Trx = 0dBm'})
+xlabel('-100m < BaseStation < 100m')
+ylabel('-100m < BaseStation < 100m')
+shading interp;
+set(gca, 'clim', [acc_dBm 0]);
+colormap([0 0 0; jet]);
+colorbar;
+plot(x_ellipse,y_ellipse,'r')
+legend('log10 distance path model', 'RaceTrack, "H=40m, L=100m"');
+hold off
+
+%% Plotting Signal Range in Shannon Engineering Building for -24dBm
+scale = 15;
+figure(8)
+hold on
+pcolor(x(N+1-scale:N+1+scale),y(N+1-scale:N+1+scale),POffice_min(N+1-scale:N+1+scale,N+1-scale:N+1+scale))
+title({'LOGPATH RECEIVED SIGNAL PLOT';'BASE-Station, OFFICE, Trx = -24dBm'})
+xlabel('-15m < BaseStation < 15m')
+ylabel('-15m < BaseStation < 15m')
+shading interp;
+set(gca, 'clim', [acc_dBm 0]);
+colormap([0 0 0; jet]);
+colorbar;
+
+%Ellipse
+a=10; % horizontal radius
+b=2;  % vertical radius 
 x0=0; % x0,y0 ellipse centre coordinates
 y0=0;
 t=-pi:0.01:pi;
 x_ellipse=x0+a*cos(t);
 y_ellipse=y0+b*sin(t);
 plot(x_ellipse,y_ellipse,'r')
-legend('log10-Distance path model', 'RaceTrack');
+legend('log10 distance path model', 'RaceTrack, "H=4m, L=20m"');
 hold off
 
-figure(7)
+figure(9)
 hold on
-pcolor(x,y,POfficeCombined)
-title('0dBm transmit power decay in office. BaseStation')
-xlabel('-42m < BaseStation < 42m') % x-axis label
-ylabel('-42m < BaseStation < 42m') % y-axis label
+pcolor(x(N+1-scale:N+1+scale),y(N+1-scale:N+1+scale),POfficeCombined_min(N+1-scale:N+1+scale,N+1-scale:N+1+scale))
+title({'LOGPATH RECEIVED SIGNAL PLOT';'COMBINED-Stations, OFFICE, Trx = -24dBm'})
+xlabel('-15m < BaseStation < 15m')
+ylabel('-15m < BaseStation < 15m')
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 colorbar;
-legend('log10-Distance path model', 'RaceTrack');
 plot(x_ellipse,y_ellipse,'r')
+legend('log10 distance path model', 'RaceTrack, "H=4m, L=20m"');
 hold off
+
 
--- ToHopOrNotToHop.m
+++ ToHopOrNotToHop.m
@@ -1,5 +1,9 @@
 clc; clear; close all;
 
+%-94dBm is antenna receiver sensitivity
+%-84dBm is chosen to give room for unexpected noise sources!
+acc_dBm = -84;            %Accepted received power
+
 R = 1;                    %Measurement Shunt-Resistance
 I_tx_max = 1*10.^(-3);    %Transmit current
 I_tx_min = 4*10.^(-6);    %I_tx_max - 24dBm
@@ -11,151 +15,218 @@ Ptx_min = I_tx_min*R^(2); %Transmit Power
 d0     = 0.01167;    %(meter) estimation of the Far Field distance for the TelosB antenna
 
 N = 200; %meter from the base station "half of a racetrack width"
-x = linspace(-N,N,N*4+1);  
-y = linspace(-N,N,N*4+1);  
+x = linspace(-N,N,N*2+1);  
+y = linspace(-N,N,N*2+1);  
 [X,Y] = meshgrid(x,y);
-%% 0dBm = max power, -24dBm = min power
+%% 0dBm = max power, -24dBm = min power, Runner running at 12km/h ? 1m/333ms
 %  AIR = 2
 gammaAIR = 2;
 % Office = 5.5 (2% of the distance) for every 5m one wall of thickness 30cm exist! 
 gammaOffice = (2*94+5.5*6)/100; %2.21
 
 %BaseStation
-d = sqrt(X.^(2)+Y.^(2)); %euclidean distance
-d(N/2+1,N/2+1) = 1;      %to avoid inf number in the center!
+d = sqrt(X.^(2)+Y.^(2));    %euclidean distance
+d((N*2)/2+1,(N*2)/2+1) = 1; %to avoid inf number in the center!
 
-PAir_W     = Ptx_max.*((d0./d).^gammaAIR); %Watt
-PAir       = 10*log10(PAir_W/0.001);    %dBm
+PAir_W = Ptx_max.*((d0./d).^gammaAIR); %Watt
+PAir   = 10*log10(PAir_W/0.001);       %dBm
 
 %NorthSation
-dNorth = sqrt((X-50).^(2)+Y.^(2));       %euclidean distance
-dNorth(N/2+1,N/2+26) = 1;                %to avoid inf number in the center!
+dNorth = sqrt((X-40).^(2)+Y.^(2)); %euclidean distance     
+dNorth((N*2)/2+1,(N*2)/2+41) = 1;  %to avoid inf number in the center!  
 
-PAirNorth_W = Ptx_max.*((d0./dNorth).^gammaAIR);                           %Watt
-PAirNorth   = 10*log10(PAirNorth_W./0.001);          %dBm
+PAirNorth_W = Ptx_max.*((d0./dNorth).^gammaAIR); %Watt
+PAirNorth   = 10*log10(PAirNorth_W./0.001);      %dBm
 
 %SouthStation
-dSouth = sqrt((X+50).^(2)+Y.^(2));       %euclidean distance
-dSouth(N/2+1,N/2-24) = 1;                %to avoid inf number in the center!
+dSouth = sqrt((X+40).^(2)+Y.^(2));  %euclidean distance
+dSouth((N*2)/2+1,(N*2)/2-39) = 1;   %to avoid inf number in the center!
 
-PAirSouth_W = Ptx_max.*((d0./dSouth).^gammaAIR);  
-PAirSouth   = 10*log10(PAirSouth_W./0.001);          %dBm
-                         %Watt
-
+PAirSouth_W = Ptx_max.*((d0./dSouth).^gammaAIR); %dBm
+PAirSouth   = 10*log10(PAirSouth_W./0.001);      %Watt         
+                         
 %Combination of stations
-PAIRCombined = 10*log10((PAir_W + PAirNorth_W + PAirSouth_W)./0.001);          %dBm
+PAIRCombined = 10*log10((PAir_W + PAirNorth_W + PAirSouth_W)./0.001); %dBm
 
 %% Fading FF=Fast fading, SF= slow fading  
-
-a=148; % horizontal radius
-b=20; % vertical radius
-x0=0; % x0,y0 ellipse centre coordinates
-y0=0;
-t=-pi:0.01:pi;
-x_ellipse=x0+a*cos(t);
-y_ellipse=y0+b*sin(t);
+%Fast Fading 250ms fast fading effect
 mu = 35+30; %+30 from dBW to dBm
 sigma = 5;
-
-%Fast Fading 350ms fast fading effect
 X_ff = normrnd(mu,sigma,[length(x),length(y)]);
 X_randff = rand(length(x),length(y)) < 0.1;
 ff = -(X_randff.*X_ff);
 
-%Slow Fading
+%North
+X_ff_north = normrnd(mu,sigma,[length(x),length(y)]);
+X_randff_north = rand(length(x),length(y)) < 0.1;
+ff_north = -(X_randff_north.*X_ff);
+
+%South
+X_ff_south = normrnd(mu,sigma,[length(x),length(y)]);
+X_randff_south = rand(length(x),length(y)) < 0.1;
+ff_south = -(X_randff_south.*X_ff);
+
+%Slow Fading 10sec effect
 X_sf = normrnd(mu,sigma,[length(x),length(y)]);
 X_randsf = rand(length(x),length(y)) < 0.1;
 X_Coor = find(X_randsf > 0.5);
 
 for i=1:length(X_Coor)
-   if X_Coor(i) < 13
-      X_Coor(i) = 13;
+   if X_Coor(i) < 22
+      X_Coor(i) = 22;
    end
 end
-X_randsf(X_Coor-12:X_Coor+12)=1; %10sec slow fading effect
+
+X_randsf(X_Coor-21:X_Coor+21)=1; %10sec slow fading effect
 sf = -(X_randsf.*X_sf);
 
-PAir_sf = PAir + sf + ff;
-PAirNorth_sf = PAirNorth + sf;
-PAirSouth_sf = PAirSouth + sf;
-PAir_W_sf      = 10.^(PAir_sf./10);
-PAirNorth_W_sf = 10.^(PAirNorth_sf./10);
-PAirSouth_W_sf = 10.^(PAirSouth_sf./10);
-PAIRCombined_sf = 10*log10((PAir_W_sf + PAirNorth_W_sf + PAirSouth_W_sf)./0.001); %dBm
+%NORTH
+X_sf_north = normrnd(mu,sigma,[length(x),length(y)]);
+X_randsf_north = rand(length(x),length(y)) < 0.1;
+X_Coor_north = find(X_randsf_north > 0.5);
+
+for i=1:length(X_Coor_north)
+   if X_Coor_north(i) < 22
+      X_Coor_north(i) = 22;
+   end
+end
+
+X_sf_north(X_Coor_north-21:X_Coor_north+21)=1; %10sec slow fading effect
+sf_north = -(X_sf_north.*X_sf_north);
+
+%SOUTH
+X_sf_south = normrnd(mu,sigma,[length(x),length(y)]);
+X_randsf_south = rand(length(x),length(y)) < 0.1;
+X_Coor_south = find(X_randsf_south > 0.5);
+
+for i=1:length(X_Coor_south)
+   if X_Coor_south(i) < 22
+      X_Coor_south(i) = 22;
+   end
+end
+
+X_sf_south(X_Coor_south-21:X_Coor_south+21)=1; %10sec slow fading effect
+sf_south = -(X_sf_south.*X_sf_south);
+
+PAir_f = PAir + sf + ff;
+PAirNorth_f = PAirNorth + sf_north + ff_north;
+PAirSouth_f = PAirSouth + sf_south + ff_south;
+PAir_W_f      = 10.^(PAir_f./10);
+PAirNorth_W_f = 10.^(PAirNorth_f./10);
+PAirSouth_W_f = 10.^(PAirSouth_f./10);
+
+PAIRCombined_sf = 10*log10((PAir_W_f + PAirNorth_W + PAirSouth_W)./0.001); %dBm
+PAIRCombined_sf_All_Noisy = 10*log10((PAir_W_f + PAirNorth_W_f + PAirSouth_W_f)./0.001); %dBm
 
 %% Plotting Signal Range in AIR for 0dBm
 figure(1)
 hold on
-pcolor(x,y,PAir_sf)
-title('0dBm transmit power decay in AIR. BaseStation')
-xlabel('-150m < BaseStation < 150m') % x-axis label
-ylabel('-150m < BaseStation < 150m') % y-axis label
+pcolor(x,y,PAir_f)
+title({'LOGPATH RECEIVED SIGNAL PLOT';'BASE-Station, AIR, Trx = 0dBm. Slow and fast fading effects from BASE ONLY'})
+xlabel('-200m < BaseStation < 200m')
+ylabel('-200m < BaseStation < 200m')
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 colorbar;
 
 %Ellipse
-a=148; % horizontal radius
-b=20; % vertical radius
-x0=0; % x0,y0 ellipse centre coordinates
+a=N-2; % horizontal radius
+b=N/5; % vertical radius
+x0=0;  % x0,y0 ellipse centre coordinates
 y0=0;
 t=-pi:0.01:pi;
 x_ellipse=x0+a*cos(t);
 y_ellipse=y0+b*sin(t);
 plot(x_ellipse,y_ellipse,'r')
+legend('log10 distance path model', 'RaceTrack, "H=80m, L=400m"');
 hold off
 
 figure(2)
 hold on
 pcolor(x,y,PAIRCombined_sf)
-title('0dBm transmit power decay in AIR. Combined Stations')
-xlabel('-150m < BaseStation < 150m') % x-axis label
-ylabel('-150m < BaseStation < 150m') % y-axis label
+title({'LOGPATH RECEIVED SIGNAL PLOT';'COMBINED-Stations, AIR, Trx = 0dBm. Slow and fast fading effects from BASE ONLY'})
+xlabel('-200m < BaseStation < 200m') % x-axis label
+ylabel('-200m < BaseStation < 200m') % y-axis label
 shading interp;
-set(gca, 'clim', [-94 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 colorbar;
 plot(x_ellipse,y_ellipse,'r')
-legend('Log-Distance path model', 'RaceTrack');
+legend('log10 distance path model', 'RaceTrack, "H=80m, L=400m"');
 hold off
-
-DF_PAIR = zeros(length(PAir_sf));
-DFCoor1_PAIR = find(PAir_sf>-94);
-DFCoor2_PAIR = find(PAir_sf<=-94);
-DF_PAIR(DFCoor1_PAIR) = -9999;
-DF_PAIR(DFCoor2_PAIR) = 0;
 
 figure(3)
 hold on
-pcolor(x,y,DF_PAIR)
-title('0dBm transmit power decay in AIR. Combined Stations')
-xlabel('-150m < BaseStation < 150m') % x-axis label
-ylabel('-150m < BaseStation < 150m') % y-axis label
+pcolor(x,y,PAIRCombined_sf_All_Noisy)
+title({'LOGPATH RECEIVED SIGNAL PLOT';'COMBINED-Stations, AIR, Trx = 0dBm. Slow and fast fading effects from ALL STATIONS'})
+xlabel('-200m < BaseStation < 200m') % x-axis label
+ylabel('-200m < BaseStation < 200m') % y-axis label
 shading interp;
-set(gca, 'clim', [-1 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [acc_dBm 0]);
 colormap([0 0 0; jet]);
 colorbar;
 plot(x_ellipse,y_ellipse,'r')
-legend('Log-Distance path model', 'RaceTrack');
+legend('log10 distance path model', 'RaceTrack, "H=80m, L=400m"');
 hold off
 
-DF_PAIR_COMB = zeros(length(PAIRCombined_sf));
-DFCoor1_PAIR_COMB = find(PAIRCombined_sf>-94);
-DFCoor2_PAIR_COMB = find(PAIRCombined_sf<=-94);
-DF_PAIR_COMB(DFCoor1_PAIR_COMB) = -9999;
-DF_PAIR_COMB(DFCoor2_PAIR_COMB) = 0;
+%% Plotting Deep Fading Binary plots
+DF_PAIR = zeros(length(PAir_f)); %Deep Fade Power AIR
+DFCoor1_PAIR = PAir_f>acc_dBm;
+DFCoor2_PAIR = PAir_f<=acc_dBm;
+DF_PAIR(DFCoor1_PAIR) = -9999;
+DF_PAIR(DFCoor2_PAIR) = 0;
 
 figure(4)
 hold on
-pcolor(x,y,DF_PAIR_COMB)
-title('0dBm transmit power decay in AIR. Combined Stations')
-xlabel('-150m < BaseStation < 150m') % x-axis label
-ylabel('-150m < BaseStation < 150m') % y-axis label
+pcolor(x,y,DF_PAIR)
+title({'BINARY DEEP FADING PLOT';'BASE-Station, AIR, Trx = 0dBm. Slow and fast fading effects from BASE ONLY'})
+xlabel('-200m < BaseStation < 200m')
+ylabel('-200m < BaseStation < 200m')
 shading interp;
-set(gca, 'clim', [-1 0]); %-94dBm is antenna receiver sensitivity
+set(gca, 'clim', [-1 0]);
 colormap([0 0 0; jet]);
 colorbar;
-plot(x_ellipse,y_ellipse,'r')
-legend('Log-Distance path model', 'RaceTrack');
+plot(x_ellipse,y_ellipse,'b')
+legend('log10 distance path model', 'RaceTrack, "H=80m, L=400m"');
+hold off
+
+DF_PAIR_COMB = zeros(length(PAIRCombined_sf));
+DFCoor1_PAIR_COMB = PAIRCombined_sf>acc_dBm;
+DFCoor2_PAIR_COMB = PAIRCombined_sf<=acc_dBm;
+DF_PAIR_COMB(DFCoor1_PAIR_COMB) = -9999;
+DF_PAIR_COMB(DFCoor2_PAIR_COMB) = 0;
+
+figure(5)
+hold on
+pcolor(x,y,DF_PAIR_COMB)
+title({'BINARY DEEP FADING PLOT';'COMBINED-Stations, AIR, Trx = 0dBm. Slow and fast fading effects from BASE ONLY'})
+xlabel('-200m < BaseStation < 200m')
+ylabel('-200m < BaseStation < 200m')
+shading interp;
+set(gca, 'clim', [-1 0]);
+colormap([0 0 0; jet]);
+colorbar;
+plot(x_ellipse,y_ellipse,'b')
+legend('log10 distance path model', 'RaceTrack, "H=80m, L=400m"');
+hold off
+
+DF_PAIR_COMB_noisy = zeros(length(PAIRCombined_sf_All_Noisy));
+DFCoor1_PAIR_COMB_Noisy = PAIRCombined_sf_All_Noisy>acc_dBm;
+DFCoor2_PAIR_COMB_Noisy = PAIRCombined_sf_All_Noisy<=acc_dBm;
+DF_PAIR_COMB_noisy(DFCoor1_PAIR_COMB_Noisy) = -9999;
+DF_PAIR_COMB(DFCoor2_PAIR_COMB_Noisy) = 0;
+
+figure(6)
+hold on
+pcolor(x,y,DF_PAIR_COMB_noisy)
+title({'BINARY DEEP FADING PLOT';'COMBINED-Stations, AIR, Trx = 0dBm. Slow and fast fading effects from ALL STATIONS'})
+xlabel('-200m < BaseStation < 200m')
+ylabel('-200m < BaseStation < 200m')
+shading interp;
+set(gca, 'clim', [-1 0]);
+colormap([0 0 0; jet]);
+colorbar;
+plot(x_ellipse,y_ellipse,'b')
+legend('log10 distance path model', 'RaceTrack, "H=80m, L=400m"');
 hold off
